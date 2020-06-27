d9b26397521858e83bc2b9b90efe6eb459d27114
Bruno Jagmin Ferreira <brunojagminferreira@gmail.com>
2020-06-26 23:26:42 -0300

    Mod 7 challenge completed

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,21 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    let income = 0;
+    let outcome = 0;
+    let total = 0;
+
+    // eslint-disable-next-line no-return-assign
+    transactions.forEach(el =>
+      el.type === 'income'
+        ? (income += Number(el.value))
+        : (outcome += Number(el.value)),
+    );
+    total = income - outcome;
+
+    return { income, outcome, total };
   }
 }
 
